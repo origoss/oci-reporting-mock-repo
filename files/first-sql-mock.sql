740cc93a9be6e7dfeba18ff28822d4548344794c
bramlak <kalmarbalint97@gmail.com>
2026-02-06 10:31:27 +0100

    feat(files): add files

---
+++ files/first-sql-mock.sql
@@ -0,0 +1,3 @@
+CREATE USER data_mock_reporting_user IDENTIFIED BY "dummy-reporting-user";
+GRANT CREATE SESSION TO data_mock_reporting_user;
+GRANT CREATE TABLE TO data_mock_reporting_user;
---
+++ files/second-sql-mock.sql
@@ -0,0 +1,3 @@
+CREATE USER data_mock_reporting_user2 IDENTIFIED BY "dummy-reporting-user2";
+GRANT CREATE SESSION TO data_mock_reporting_user2;
+GRANT CREATE TABLE TO data_mock_reporting_user2;
